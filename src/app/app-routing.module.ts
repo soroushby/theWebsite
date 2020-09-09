1282b5b87992732c1b53cef832489803f7d6b331
soroushby <49848393+soroushby@users.noreply.github.com>
2020-09-09 02:50:53 -0700

    desgning home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,13 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
-
+  {
+    path: 'portfolios',
+    loadChildren: () =>
+      import('./features/portfolios/portfolios.module').then(
+        (m) => m.PortfoliosModule
+      ),
+  },
   { path: '**', component: PageNotFoundComponent },
 ];
 
